4f67c55ef18b0649389d67af0b190add0b2781df
Joe Cosentino <jac5384@gmail.com>
2020-04-17 17:33:33 -0400

    Initial commit

---
+++ precache-manifest.50d0d21c263e9b5cd19730d9210e859d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8a357a10cf435e7f24ebd20a72fc5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922b5e325e447b85658a",
+    "url": "/static/css/main.83384982.chunk.css"
+  },
+  {
+    "revision": "d6fdaf45d2666e799bd3",
+    "url": "/static/js/2.acebadce.chunk.js"
+  },
+  {
+    "revision": "922b5e325e447b85658a",
+    "url": "/static/js/main.4c4b9dbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
